0778fff44d396c8f0be1ea7ada53fd4307f32ade
Rosie Mary <laltnadi@gmail.com>
2022-09-20 21:48:23 +0630

    livewireWithDatabase

--- app/Http/Livewire/User.php
+++ app/Http/Livewire/User.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User as Users;
 use Livewire\Component;
 
 class User extends Component
 {
+    public $users;
+    
     public function render()
     {
-        return <<<'blade'
-            <div>
-                <h2>This is livewire inline component.</h2>
-            </div>
-        blade;
+        $this->users = Users::all();
+        return view('livewire.user');
     }
 }
---
+++ resources/views/livewire/user.blade.php
@@ -0,0 +1,35 @@
+<div>
+    <section style="padding-top: 50px">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3>User List Showing</h3>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>E-mail</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($users as $user)
+                                        <tr>
+                                            <td>{{ $user->id }}</td>
+                                            <td>{{ $user->name }}</td>
+                                            <td>{{ $user->email }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
